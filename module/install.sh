466816a4360c70556313bc6e662dfb3b6a9c4f23
HERAULT-MARTIN Pierre <HicksCorp@GMail.com>
2015-09-11 12:15:00 +0100

    Copies the source tree to /usr/src rather than symlinking it.

--- module/install.sh
+++ module/install.sh
@@ -5,12 +5,14 @@ VER="1.0"
 PWD=`pwd`
 
 # Copy sources...
-ln -s $PWD /usr/src/$MOD-$VER
+rm -rf . /usr/src/$MOD-$VER
+cp -R  . /usr/src/$MOD-$VER
 
 dkms add      -m $MOD -v $VER
 dkms build    -m $MOD -v $VER
 dkms mkdeb    -m $MOD -v $VER
-
+dkms remove   -m $MOD -v $VER
 #dkms install  -m $MOD -v $VER
+
 
 modprobe $MOD
